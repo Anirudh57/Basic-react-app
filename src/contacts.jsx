25fadda923ef1b79268c306618d15145f2bc5ef3
Vishal Vishwakarma <91001376+Anirudh57@users.noreply.github.com>
2023-06-15 13:22:39 +0530

    Add files via upload

---
+++ src/contacts.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const contacts = [{
+        name: "Shinchan",
+        imgURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR071jbwHIeEllX_SppwuZsGLSt7QsPAiff8g&usqp=CAU",
+        phone: "+123 456 789",
+        email: "shinchan@Nohara.com"
+    },
+    {
+        name: "Doraemon",
+        imgURL: "./images.png",
+        phone: "+987 654 321",
+        email: "Doraemon@Cat.com"
+    },
+    {
+        name: "Tom & Jerry",
+        imgURL: "./tom.png",
+        phone: "+918 372 574",
+        email: "Tom@Jerry.com"
+    }
+];
+
+export default contacts;
